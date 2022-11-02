58788400662f951a8e749303bed033a4cd76cb53
Andrew Douglas <andrew.a.douglas@gmail.com>
2022-11-02 15:34:52 -0400

    more functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import { useState } from 'react';
 
 function App(props) {
-  const [proteinGrams, setProteinGrams] = useState(0);
-  const [carbGrams, setCarbGrams] = useState(0);
-  const [fatGrams, setFatGrams] = useState(0);
+  const [proteinGrams, setProteinGrams] = useState(160);
+  const [carbGrams, setCarbGrams] = useState(210);
+  const [fatGrams, setFatGrams] = useState(71);
 
   let proteinCals = proteinGrams * 4;
   let carbCals = carbGrams * 4;
   let fatCals = fatGrams * 9;
   let totalCals = proteinCals + carbCals + fatCals;
+  let mealsArray = [2,3,4,5,6,7,8];
+
+  const mapArray = (arr) => {
+    arr.map(item => console.log('test'))
+  }
 
   const handleChangeProteinGrams = (event) => {
     setProteinGrams(() => event.target.value);
@@ -46,6 +51,13 @@ function App(props) {
       </div>
       <div className='totals'>
         <p>Total Calories are {totalCals}</p>
+      </div>
+      <div className='meals'>
+        <p>3 Meals a Day might look like: {Math.round(proteinGrams / 3) + 'P'}/{Math.round(carbGrams / 3) + 'C'}/{Math.round(fatGrams / 3) + 'F'} = {Math.round(totalCals / 3) + 'cal'}</p>
+        <p>4 Meals a Day might look like: {Math.round(proteinGrams / 4) + 'P'}/{Math.round(carbGrams / 4) + 'C'}/{Math.round(fatGrams / 4) + 'F'} = {Math.round(totalCals / 4) + 'cal'}</p>
+        <p>5 Meals a Day might look like: {Math.round(proteinGrams / 5) + 'P'}/{Math.round(carbGrams / 5) + 'C'}/{Math.round(fatGrams / 5) + 'F'} = {Math.round(totalCals / 5) + 'cal'}</p>
+        <p>6 Meals a Day might look like: {Math.round(proteinGrams / 6) + 'P'}/{Math.round(carbGrams / 6) + 'C'}/{Math.round(fatGrams / 6) + 'F'} = {Math.round(totalCals / 6) + 'cal'}</p>
+        {mapArray(mealsArray)}
       </div>
       
     </div>
