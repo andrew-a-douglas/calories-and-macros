458cd9988f7d21da44dd073d2d9f89c684295669
Andrew Douglas <andrew.a.douglas@gmail.com>
2022-11-02 14:32:30 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function App(props) {
+  const {calories, setCalories} = useState(null);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calories and Macros</h1>
+      <input placeholder='Calories' type='text' value={calories} />
     </div>
   );
 }
